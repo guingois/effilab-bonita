8ed0aa4c74a8fffdd86fa0d7c6152b63f034f786
Pierre Deville <pierre.deville@effilab.com>
2017-10-23 15:52:13 +0200

    Add BusinessData search

--- lib/bonita/resources/bdm/business_data_resource.rb
+++ lib/bonita/resources/bdm/business_data_resource.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 module Bonita
   module Bdm
-    # API reference : http://documentation.bonitasoft.com/?page=bdm-api#toc1
+    # API reference : http://documentation.bonitasoft.com/?page=bdm-api
     class BusinessDataResource < ResourceKit::Resource
       include ErrorHandler
 
       resources do
         action :find do
           path 'bonita/API/bdm/businessData/:businessDataType/:persistenceId'
+          verb :get
+          handler(200) { |response| JSON.parse response.body }
+        end
+
+        action :search do
+          path 'bonita/API/bdm/businessData/:businessDataType'
+          query_keys :c, :f, :q, :p
           verb :get
           handler(200) { |response| JSON.parse response.body }
         end
