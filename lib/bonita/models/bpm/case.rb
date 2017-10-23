c6a7d4c2a4a09a548fd0d372364580f61435bef9
Pierre Deville <pierre.deville@effilab.com>
2017-10-23 15:53:27 +0200

    Handle Bpm/Case resource

---
+++ lib/bonita/models/bpm/case.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal : true
+module Bonita
+  module Bpm
+    class Case < BaseModel
+      attribute :activeFlowNodes
+      attribute :end_date
+      attribute :failedFlowNodes
+      attribute :id
+      attribute :last_update_date
+      attribute :processDefinitionId
+      attribute :rootCaseId
+      attribute :searchIndex1Label
+      attribute :searchIndex1Value
+      attribute :searchIndex2Label
+      attribute :searchIndex2Value
+      attribute :searchIndex3Label
+      attribute :searchIndex3Value
+      attribute :searchIndex4Label
+      attribute :searchIndex4Value
+      attribute :searchIndex5Label
+      attribute :searchIndex5Value
+      attribute :start
+      attribute :started_by
+      attribute :startedBySubstitute
+      attribute :state
+    end
+  end
+end
---
+++ lib/bonita/resources/bpm/case_resource.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+module Bonita
+  module Bpm
+    # API reference : http://documentation.bonitasoft.com/?page=bpm-api#toc27
+    class CaseResource < ResourceKit::Resource
+      include ErrorHandler
+
+      resources do
+
+        action :find do
+          path 'bonita/API/bpm/case/:caseId'
+          verb :get
+          handler(200) { |response| CaseMapping.extract_single(response.body, :read) }
+        end
+
+        action :create do
+          path 'bonita/API/bpm/case/'
+          verb :post
+          body { |object| CaseMapping.safe_representation_for(:create, object) }
+          handler(200) { |response| CaseMapping.extract_single(response.body, :read) }
+        end
+
+        action :search do
+          query_keys :s, :f, :o, :d, :c
+          path 'bonita/API/bpm/case'
+          verb :get
+          handler(200) { |response, payload| Bonita::Utils::SearchHandler.new(response, payload, self).call }
+        end
+
+        action :delete do
+          path 'bonita/API/bpm/case/:caseId'
+          verb :delete
+          handler(200) { true }
+        end
+
+        action :delete_bulk do
+          path 'bonita/API/bpm/case/'
+          verb :delete
+          body { |object| object.to_json }
+          handler(200) { true }
+        end
+
+        action :context do
+          path 'bonita/API/bpm/case/:caseId/context'
+          verb :get
+          handler(200) { |response| CaseMapping.extract_single(response.body, :read) }
+        end
+      end
+    end
+  end
+end
